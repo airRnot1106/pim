5f17cf39bfd5476e30c01ef16cf1217c2278cad1
airRnot1106 <yngw.rintaro@gmail.com>
2024-12-15 23:20:07 +0900

    build(nix): update rust platform configuration in pim.nix

--- nix/pim.nix
+++ nix/pim.nix
@@ -1,4 +1,11 @@
-{ rustPlatform }:
+{ makeRustPlatform, rust-bin }:
+let
+  toolchain = rust-bin.stable.latest.default;
+  rustPlatform = makeRustPlatform {
+    cargo = toolchain;
+    rustc = toolchain;
+  };
+in
 rustPlatform.buildRustPackage {
   pname = "pim";
   version = "0.1.0";
